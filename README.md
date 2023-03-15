95afdf50cdab3daf14dd35341b1d548c845747b2
Easy <easychen@qq.com>
2023-03-15 18:17:42 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# api2d-js
-js sdk for api2d
+中文说明见后边，以下英文由GPT3.5友情翻译。
+
+# A simple pure browser SDK for Api2d and OpenAI
+
+For some reason, I couldn't find a pure browser implementation of the OpenAI SDK, they were all designed for Node. So, I decided to create my own implementation that is compatible with both OpenAI and [API2d](https://api2d.com/) keys.
+
+## Usage
+
+```js
+const api = new Api2d(key, apiBaseUrl);
+
+// Chat completion
+const ret = await api.completion({
+    model:'gpt-3.5-turbo',
+    messages: [
+        {
+            "role":"user",
+            "content":"Hello"
+        }
+    ],
+    stream: true, // Supports streaming, note that when stream is true, the return value is undefined
+    onMessage: (string)=> {
+        console.log( "SSE returned, the complete string received is:", string );
+    },
+    onEnd: (string)=> {
+        console.log( "end", string );
+    }
+});
+
+
+// Embeddings
+const ret = await api.embeddings({
+    input: "hello world"
+});
+console.log( ret );
+
+api.setKey( 'newkey' ); // set key
+api.setApiBaseUrl( 'https://...your openai proxy address' );
+
+```
+
+
+# 一个简单的纯浏览器SDK for Api2d 和 OpenAI
+
+不知道为啥，找了半天没有找到纯 Browser 的 OpenAI SDK，都是 Node 实现的。于是自己写了一个，同时兼容 OpenAI 和 [API2d](https://api2d.com/) 的key。
+
+## 使用方法
+
+```js
+const api = new Api2d(key, apiBaseUrl);
+
+// chat 补全
+const ret = await api.completion({
+    model:'gpt-3.5-turbo',
+    messages: [
+        {
+            "role":"user",
+            "content":"Hello"
+        }
+    ],
+    stream: true, // 支持 stream，注意stream为 true 的时候，返回值为undefined
+    onMessage: (string)=> {
+        console.log( "SSE返回，这里返回的是已经接收到的完整字符串", string );
+    },
+    onEnd: (string)=> {
+        console.log( "end", string );
+    }
+});
+
+
+// embeddings
+const ret = await api.embeddings({
+    input: "hello world"
+});
+console.log( ret );
+
+api.setKey( 'newkey' ); // set key
+api.setApiBaseUrl( 'https://...your openai proxy address' );
+
+```
+
---
+++ index.js
@@ -0,0 +1,94 @@
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+
+export default class Api2d
+{
+    // 设置key和apiBaseUrl
+    constructor( key = null, apiBaseUrl=null )
+    {
+        this.key = key;
+        this.apiBaseUrl = apiBaseUrl || ( key?.startsWith("fk") ? "https://openai.api2d.net":"https://api.openai.com" );
+    }
+
+    // set key
+    setKey( key )
+    {
+        this.key = key;
+    }
+
+    // set apiBaseUrl
+    setApiBaseUrl( apiBaseUrl )
+    {
+        this.apiBaseUrl = apiBaseUrl;
+    }
+
+    // Completion
+    async completion( options )
+    {
+        // 拼接目标URL
+        const url = this.apiBaseUrl + "/v1/chat/completions";
+        // 拼接headers
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + this.key
+        };
+
+        const { onMessage, onEnd, model,  ...restOptions } = options;
+
+        // 如果是流式返回，且有回调函数
+        if( restOptions.stream && onMessage )
+        {
+            let chars = "";
+            // console.log( "in stream" );
+            const response = await fetchEventSource( url, {
+                method: "POST",
+                headers: {...headers, "Accept": "text/event-stream"},
+                body: JSON.stringify( {...restOptions, model:model||'gpt-3.5-turbo'}),
+                onmessage: e => {
+                    if( e.data == '[DONE]' )
+                    {
+                        // console.log( 'DONE' );
+                        if( onEnd ) onEnd( chars );
+                        chars = "";
+                    }else
+                    {
+                        // console.log( e.data );
+                        const event = JSON.parse(e.data);
+                        if( event.choices[0].delta.content ) chars += event.choices[0].delta.content;
+                        onMessage( chars );
+                    }
+                    
+                },
+            });
+        }else
+        {
+            // 使用 fetch 发送请求
+            const response = await fetch( url, {
+                method: "POST",
+                headers: headers,
+                body: JSON.stringify( restOptions )
+            });
+            return await response.json();
+        }
+    }
+
+    async embeddings( options )
+    {
+        // 拼接目标URL
+        const url = this.apiBaseUrl + "/v1/embeddings";
+        // 拼接headers
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + this.key
+        };
+        const {model,  ...restOptions } = options;
+        // 使用 fetch 发送请求
+        const response = await fetch( url, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify( {...restOptions,model:model||'text-embedding-ada-002'} )
+        });
+        return await response.json();
+    }
+}
+
+
